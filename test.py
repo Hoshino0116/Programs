275c1679d09accc5268bede59b56a1b831cd3efb
Hoshino0116 <60649916+Hoshino0116@users.noreply.github.com>
2020-03-06 21:42:18 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,29 +1,27 @@
-import sys
-
-f = open('input.txt')
-line = f.readline()
-l_list = []
-while line:
-	l = line.split(':')
-	if len(l) == 1:
-		m = int(l[0])
-		break
-	l_list.append(l)
-	line = f.readline()
-f.close
+H,N= map(int,input().split())
 
 a = []
 b = []
-for n in range(len(l_list)):
-	if m % int(l_list[n][0]) == 0:
-		a.append(int(l_list[n][0]))
-		b.append(l_list[n][1])
-if len(a) == 0:
-	print(m)
-	sys.exit()
 
-c = zip(a,b)
-c = sorted(c)
+for i in range(N):
+  a_dash,b_dash = map(int,input().split())
+  a.append(a_dash)
+  b.append(b_dash)
 
-for i,s in c:
-	print(s.rstrip('\n'),end="")
+#dp[i+1][j] = i番目までの魔法で体力jのモンスターに
+#勝つことができる、魔力の最小値
+INF = 10**9
+dp = [[INF for _ in range(H+1)] for _ in range(N+1)]
+
+#体力0のモンスターは初めからデスしている
+for i in range(N+1):
+  dp[i][0] = 0
+
+for i in range(N):
+  for j in range(H+1):
+    if a[i] > j:
+      dp[i+1][j] = min(dp[i][j],b[i])
+    else:
+      dp[i+1][j] = min(dp[i][j],dp[i+1][j-a[i]]+ b[i])
+
+print(dp[N][H])
